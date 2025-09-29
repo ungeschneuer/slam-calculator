8fa67ec49b428fe1f75c1b5846bfea4c011c177d
ungeschneuer <33438600+ungeschneuer@users.noreply.github.com>
2025-09-30 01:40:52 +0200

    skript fix

--- src/version-info.js
+++ src/version-info.js
@@ -1,13 +1,13 @@
 
 // Auto-generated version info - DO NOT EDIT
 export const VERSION_INFO = {
-    version: '1.3.1+34cc4021',
-    build: 1759188902744,
-    buildId: '34cc4021',
-    hash: '2d3876da',
-    timestamp: '2025-09-29T23:35:02.744Z',
-    generated: '2025-09-29T23:35:02.745Z'
+    version: '1.3.1+f8d6c6e1',
+    build: 1759189221696,
+    buildId: 'f8d6c6e1',
+    hash: '208c03c8',
+    timestamp: '2025-09-29T23:40:21.696Z',
+    generated: '2025-09-29T23:40:21.698Z'
 };
 
-export const CACHE_VERSION = 'v2d3876da';
-export const CACHE_NAME = 'poetry-slam-calculator-2d3876da';
+export const CACHE_VERSION = 'v208c03c8';
+export const CACHE_NAME = 'poetry-slam-calculator-208c03c8';
